29974957fd74a2650abe9c8f78b782d0f192601a
ahdirmai <ridhofahmij225@gmail.com>
2026-04-18 08:19:53 +0800

    change broadcasting to pusher.com

--- resources/js/echo.ts
+++ resources/js/echo.ts
@@ -4,7 +4,7 @@ import Pusher from 'pusher-js';
 declare global {
     interface Window {
         Pusher: typeof Pusher;
-        Echo: Echo<'reverb'>;
+        Echo: Echo<'pusher'>;
     }
 }
 
@@ -12,18 +12,9 @@ if (typeof window !== 'undefined') {
     window.Pusher = Pusher;
 
     window.Echo = new Echo({
-        broadcaster: 'reverb',
-        key: import.meta.env.VITE_REVERB_APP_KEY as string,
-        wsHost:
-            import.meta.env.VITE_REVERB_HOST === '127.0.0.1'
-                ? window.location.hostname
-                : (import.meta.env.VITE_REVERB_HOST as string),
-        wsPort: Number(import.meta.env.VITE_REVERB_PORT ?? 8080),
-        wssPort: Number(import.meta.env.VITE_REVERB_PORT ?? 8080),
-        scheme: (import.meta.env.VITE_REVERB_SCHEME as string) ?? 'http',
-        forceTLS:
-            ((import.meta.env.VITE_REVERB_SCHEME as string) ?? 'http') ===
-            'https',
-        enabledTransports: ['ws', 'wss'],
+        broadcaster: 'pusher',
+        key: import.meta.env.VITE_PUSHER_APP_KEY as string,
+        cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER as string,
+        forceTLS: true,
     });
 }
