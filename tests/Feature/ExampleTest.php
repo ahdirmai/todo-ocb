0a5d223ddcd72b6545ec9a195d4785bf22fe9822
ahdirmai <ridhofahmij225@gmail.com>
2026-04-21 16:45:47 +0800

    Change Web Name

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,12 @@
 <?php
 
 test('returns a successful response', function () {
+    config()->set('session.driver', 'array');
+    config()->set('app.name', 'OCB KPI Management');
+
     $response = $this->get(route('home'));
+
+    expect(config('app.name'))->toBe('OCB KPI Management');
 
     $response->assertOk();
 });
